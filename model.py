c80008999a28947f82599beb7f1d6eba8139e1e3
ingfedericobollano <s234201@studenti.polito.it>
2020-12-27 17:44:22 +0100

    Last push test

--- model.py
+++ model.py
@@ -8,30 +8,38 @@ class Permissions (db.Model):
 
     def __repr__(self):
         return "<Role %r>" % self.name
-
-'''
-    #'citta'
-class City (db.Model):
-    name                = db.Column(db.String(50),primary_key=True)
-    otherInfo           = db.Column(db.Integer,nullable=False)
-    otherInfo2          = db.Column(db.String(50),nullable=False)
-
-    def __repr__(self):
-        return "<City %r>" % self.name
-        '''
+''''
     #'Ateneo'
 class University(db.Model):
-    idUniversity        = db.Column(db.String(50), primary_key=True)
-    Name                = db.Column(db.String(50), nullable=False)
-    NameShort          = db.Column(db.String(50), nullable=True)
-    State          = db.Column(db.String(50), nullable=False)
-    Address          = db.Column(db.String(50), nullable=False)
-    Dean          = db.Column(db.String(50), nullable=False)
-    Website          = db.Column(db.String(50), nullable=False)
+    idUniversity        = db.Column(db.Integer, primary_key=True)
+    name                = db.Column(db.String(200), nullable=False)
+    nickname            = db.Column(db.String(200), nullable=True)
+    state               = db.Column(db.String(200), nullable=False)
+    status              = db.Column(db.String(50))
+    stataleLibera       = db.Column(db.String(50))
+    geogrZone           = db.Column(db.String(50))
+    address             = db.Column(db.String(50))
+    dean                = db.Column(db.String(50))
+    website             = db.Column(db.String(50))
+    numberOfStudents    = db.Column(db.Integer)
 
     def __repr__(self):
         return "<University %r>" % self.name
-'''
+
+class Exam (db.Model):
+    idExam              = db.Column(db.Integer,primary_key=True)
+    name                = db.Column(db.String(50),unique=True,nullable=False)
+    professor           = db.Column(db.String(50),nullable=False)
+    assistant1          = db.Column(db.String(50), nullable=False)
+    assistant2          = db.Column(db.String(50),nullable=False)
+    CFU                 = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "<Exam %r>" % self.Name, self.professor
+
+
+
+
      #'Corso di laurea - Politecnico di Torino Ing Gest'
 class Program (db.Model):
     idProgram           = db.Column(db.String(50),primary_key=True)  #Foreign? Mix da creare
@@ -42,7 +50,7 @@ class Program (db.Model):
 
     def __repr__(self):
         return "<Program %r>" % self.nameProgram, self.descriptionProgram
-'''
+
     #'classe di laurea - L8 LM31 ..'
 class Classification(db.Model):
     idClass             = db.Column(db.String(50),primary_key=True)
@@ -52,7 +60,7 @@ class Classification(db.Model):
 
     def __repr__(self):
         return "<Class %r>" % self.idClass, self.name
-'''
+
     #'Esame'
 class Esame (db.Model):
     idExam              = db.Column(db.String(50),primary_key=True)
@@ -91,8 +99,6 @@ class RelationReview (db.Model):
     idUniversity        = db.Column(db.String(200),db.ForeignKey('University.name'))
     idProgram           = db.Column(db.String(200),db.ForeignKey('Program.idProgram'))
     idExam              = db.Column(db.String(200),db.ForeignKey('Exam.idExam'))
-
-
 '''
 
 
